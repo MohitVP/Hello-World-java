e9e8ac09105b84c12f9f652197aa745304cea6d4
Mohit Pathak <124071093+MohitVP@users.noreply.github.com>
2025-05-22 15:59:46 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+     System.out.println("Hello, KrishnaDevOps");
+     System.out.println("Hello, AWSB05DevOps_AWS011");
+     System.out.println("Hello, Jenkinsteams"); 
+
+  }
+}
