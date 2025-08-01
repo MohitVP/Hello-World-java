13affdfd0a3c65af40d7e592e07e48d5e9efe0a7
Mohit Pathak <124071093+MohitVP@users.noreply.github.com>
2025-08-01 13:50:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, KrishnaDevOps");
      System.out.println("Hello, AWSB05DevOps_AWS011");
-     System.out.println("Hello, Jenkinsteams"); 
+     System.out.println("Hello, HelloJenkins"); 
 
   }
 }
